6cf82e719d74c452828ed17446fe8463863a54d5
Aman <67479108+Bezdarnost@users.noreply.github.com>
2022-10-22 21:08:44 +0600

    Delete 1674b.cpp

--- 1674b.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-    int t;string s;
-    for(cin>>t;t--;){
-        cin>>s;
-        cout<<(int(s[0])-97)*25+(int(s[1])-97)+(s[0]>s[1])<<endl;
-    }
-}
